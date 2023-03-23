df6718251d18b7d1e7443b41a77195effa157a8c
Peter Chibunna <peterchibunna@gmail.com>
2023-03-23 12:50:58 +0100

    Updated base structure

--- main/cors.py
+++ main/cors.py
@@ -36,7 +36,8 @@ from tastypie.exceptions import (
 	UnsupportedSerializationFormat, UnsupportedDeserializationFormat,
 )
 from tastypie.utils import (
-	dict_strip_unicode_keys, is_valid_jsonp_callback_value, string_to_python,
+	# dict_strip_unicode_keys,
+	is_valid_jsonp_callback_value, string_to_python,
 	trailing_slash,
 )
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
@@ -48,36 +49,36 @@ class ResourceMixin:
 
 class ModelResource(ResourceMixin, resources.ModelResource):
 
-	def build_filters(self, filters=None, ignore_bad_filters=False):
-		""" Overrides BaseModelResource.build_filters to support query terms for related fields """
-
-		if filters is None:
-			filters = {}
-
-		qs_filters = {}
-
-		for filter_expr, value in filters.items():
-			filter_bits = filter_expr.split(LOOKUP_SEP)
-			field_name = filter_bits.pop(0)
-
-			if field_name not in self.fields:
-				# It's not a field we know about. Move along citizen.
-				continue
-
-			try:
-				filter_type = self.resolve_filter_type(field_name, filter_bits, 'exact')
-				lookup_bits = self.check_filtering(field_name, filter_type, filter_bits)
-			except InvalidFilterError:
-				if ignore_bad_filters:
-					continue
-				else:
-					raise
-			value = self.filter_value_to_python(value, field_name, filters, filter_expr, filter_type)
-
-			qs_filter = LOOKUP_SEP.join(lookup_bits)
-			qs_filters[qs_filter] = value
-
-		return dict_strip_unicode_keys(qs_filters)
+	# def build_filters(self, filters=None, ignore_bad_filters=False):
+	# 	""" Overrides BaseModelResource.build_filters to support query terms for related fields """
+	#
+	# 	if filters is None:
+	# 		filters = {}
+	#
+	# 	qs_filters = {}
+	#
+	# 	for filter_expr, value in filters.items():
+	# 		filter_bits = filter_expr.split(LOOKUP_SEP)
+	# 		field_name = filter_bits.pop(0)
+	#
+	# 		if field_name not in self.fields:
+	# 			# It's not a field we know about. Move along citizen.
+	# 			continue
+	#
+	# 		try:
+	# 			filter_type = self.resolve_filter_type(field_name, filter_bits, 'exact')
+	# 			lookup_bits = self.check_filtering(field_name, filter_type, filter_bits)
+	# 		except InvalidFilterError:
+	# 			if ignore_bad_filters:
+	# 				continue
+	# 			else:
+	# 				raise
+	# 		value = self.filter_value_to_python(value, field_name, filters, filter_expr, filter_type)
+	#
+	# 		qs_filter = LOOKUP_SEP.join(lookup_bits)
+	# 		qs_filters[qs_filter] = value
+	#
+	# 	return dict_strip_unicode_keys(qs_filters)
 
 	def check_filtering(self, field_name, filter_type='exact', filter_bits=None):
 		""" Overrides BaseModelResource.check_filtering to work with self.build_filters above """
@@ -93,7 +94,7 @@ class ModelResource(ResourceMixin, resources.ModelResource):
 			# Must be an explicit whitelist.
 			if filter_type not in self._meta.filtering[field_name]:
 				raise InvalidFilterError("'%s' is not an allowed filter on the '%s' field." % (filter_type, field_name))
-		
+
 		if self.fields[field_name].attribute is None:
 			raise InvalidFilterError("The '%s' field has no 'attribute' for searching with." % field_name)
 
@@ -158,18 +159,18 @@ class BaseCorsResource(Resource):
 	"""
 	Class implementing CORS
 	"""
-	
+
 	def error_response(self, *args, **kwargs):
 		response = super(BaseCorsResource, self).error_response(*args, **kwargs)
 		return self.add_cors_headers(response, expose_headers=True)
-	
+
 	def add_cors_headers(self, response, expose_headers=False):
 		response['Access-Control-Allow-Origin'] = '*'
 		response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
 		if expose_headers:
 			response['Access-Control-Expose-Headers'] = 'Location'
 		return response
-	
+
 	def create_response(self, *args, **kwargs):
 		"""
 		Create the response for a resource. Note this will only
@@ -178,7 +179,7 @@ class BaseCorsResource(Resource):
 		"""
 		response = super(BaseCorsResource, self).create_response(*args, **kwargs)
 		return self.add_cors_headers(response)
-	
+
 	def post_list(self, request, **kwargs):
 		"""
 		In case of POST make sure we return the Access-Control-Allow Origin
@@ -187,7 +188,7 @@ class BaseCorsResource(Resource):
 		# logger.debug("post list %s\n%s" % (request, kwargs));
 		response = super(BaseCorsResource, self).post_list(request, **kwargs)
 		return self.add_cors_headers(response, True)
-	
+
 	def post_detail(self, request, **kwargs):
 		"""
 		In case of POST make sure we return the Access-Control-Allow Origin
@@ -196,7 +197,7 @@ class BaseCorsResource(Resource):
 		# logger.debug("post detail %s\n%s" (request, **kwargs));
 		response = super(BaseCorsResource, self).post_list(request, **kwargs)
 		return self.add_cors_headers(response, True)
-	
+
 	def put_list(self, request, **kwargs):
 		"""
 		In case of PUT make sure we return the Access-Control-Allow Origin
@@ -204,21 +205,21 @@ class BaseCorsResource(Resource):
 		"""
 		response = super(BaseCorsResource, self).put_list(request, **kwargs)
 		return self.add_cors_headers(response, True)
-	
+
 	def put_detail(self, request, **kwargs):
 		response = super(BaseCorsResource, self).put_detail(request, **kwargs)
 		return self.add_cors_headers(response, True)
-	
+
 	def method_check(self, request, allowed=None):
 		"""
 		Check for an OPTIONS request. If so return the Allow- headers
 		"""
 		if allowed is None:
 			allowed = []
-		
+
 		request_method = request.method.lower()
 		allows = ','.join(map(lambda s: s.upper(), allowed))
-		
+
 		if request_method == 'options':
 			response = HttpResponse(allows)
 			response['Access-Control-Allow-Origin'] = '*'
@@ -226,21 +227,21 @@ class BaseCorsResource(Resource):
 			response['Access-Control-Allow-Methods'] = "GET, PUT, POST, PATCH, OPTIONS"
 			response['Allow'] = allows
 			raise ImmediateHttpResponse(response=response)
-		
+
 		if request_method not in allowed:
 			response = http.HttpMethodNotAllowed(allows)
 			response['Allow'] = allows
 			raise ImmediateHttpResponse(response=response)
-		
+
 		return request_method
-	
+
 	def wrap_view(self, view):
 		@csrf_exempt
 		def wrapper(request, *args, **kwargs):
 			request.format = kwargs.pop('format', None)
 			wrapped_view = super(BaseCorsResource, self).wrap_view(view)
 			return wrapped_view(request, *args, **kwargs)
-		
+
 		return wrapper
 
 
@@ -249,7 +250,7 @@ class CORSModelResource(BaseCorsResource, ModelResource):
 	def __init__(self, api_name=None):
 		super().__init__(api_name=None)
 		self.specified_fields = []
-	
+
 	def get_object_list(self, request):
 		# import time
 		# time.sleep(2)
@@ -264,7 +265,7 @@ class CORSModelResource(BaseCorsResource, ModelResource):
 			self.specified_fields = fields.split(',')
 		except:
 			self.specified_fields.append(fields)
-		
+
 		# make `distinct` default for m2m filters
 		has_m2m = False
 		for field in filters:
@@ -274,78 +275,78 @@ class CORSModelResource(BaseCorsResource, ModelResource):
 				related = False
 			if related and related.get_internal_type() == 'ManyToManyField':
 				has_m2m = True
-		
+
 		only_fields = []
 		select_related = []
-		
+
 		for specified_field in self.specified_fields:
-			
+
 			try:
 				fields = specified_field.split('__')
 			except:
 				continue
-			
+
 			# Only adds fields that exist for this model
 			# excluding model methods
 			for meta_field in objects.model._meta.fields:
-				
+
 				if meta_field.name == fields[0]:
 					only_fields.append(specified_field)
-			
+
 			# Set `select_related` for related fields
 			if len(fields) > 1:
 				select_related.append('__'.join(fields[0:len(fields) - 1]))
-		
+
 		if len(only_fields):
 			objects = objects.only(*only_fields)
-		
+
 		if len(self._meta.excludes):
 			objects = objects.defer(*self._meta.excludes)
-		
+
 		if len(select_related):
 			objects = objects.select_related(*select_related)
-		
+
 		if (has_m2m and not distinct) or distinct:
 			objects = objects.distinct()
-		
+
 		return objects
-	
+
 	def full_dehydrate(self, bundle, for_list=False):
-		
+
 		"""
 		This override disables `full=True` and other things we don't use
 		"""
-		
+
 		# call the base class if qs param `fields` is not set
 		if not len(self.specified_fields):
 			return super().full_dehydrate(bundle, for_list)
-		
+
 		# Dehydrate each field supplied in the `fields` parameter
 		for field_name, field_object in self.fields.items():
-			
+
 			# A touch leaky but it makes URI resolution work.
 			if getattr(field_object, 'dehydrated_type', None) == 'related':
 				field_object.api_name = self._meta.api_name
 				field_object.resource_name = self._meta.resource_name
-			
+
 			# Check for an optional method to do further dehydration.
 			method = getattr(self, "dehydrate_%s" % field_name, None)
-			
+
 			if method:
 				bundle.data[field_name] = method(bundle)
-		
+
 		bundle = self.dehydrate(bundle)
 		return bundle
-	
+
 	def dehydrate(self, bundle):
-		
+
 		# Dehydrate each field including related ones
 		for row in self.specified_fields:
-			
+
 			f = row.split('__')
-			
+
 			bundle.data[row] = reduce(getattr, f, bundle.obj)
-			
+
 			# display actual values for `choices` fields
 			method = getattr(bundle.obj, "get_%s_display" % f[0], False)
 			if method:
--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Election(models.Model):
+	name = models.CharField(max_length=75)
+	active = models.BooleanField(default=True)
+
+
+class State(models.Model):
+	name = models.CharField(max_length=100)
+	location = models.PointField(null=True, geography=True)
+
+
+class Party(models.Model):
+	abbr = models.CharField(max_length=5, unique=True)
+	# we will use the `abbr` attribute to name the images
+	name = models.CharField(max_length=100)
+
+
+class Vote(models.Model):
+	party = models.ForeignKey(Party, on_delete=models.CASCADE)
+	total = models.IntegerField(default=0)
+	state = models.ForeignKey(State, on_delete=models.CASCADE)
+	election = models.ForeignKey('Election', on_delete=models.CASCADE)
